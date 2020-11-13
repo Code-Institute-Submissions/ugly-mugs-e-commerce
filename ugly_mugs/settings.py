d39e7a6cda4e5716636419791d9013b0b3ff230f
Robin Westerback <robinwesterback1989@gmail.com>
2020-11-13 15:56:14 +0000

    Update allowed hosts

--- ugly_mugs/settings.py
+++ ugly_mugs/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://ugly-mugs.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['ugly-mugs.herokuapp.com', 'localhost']
 
 
 # Application definition
