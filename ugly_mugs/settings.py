dc1c3ebe0ecf0091be37c50ab353bef880379a6b
Robin Westerback <robinwesterback1989@gmail.com>
2020-11-16 18:17:52 +0000

    Add home app and templates

--- ugly_mugs/settings.py
+++ ugly_mugs/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     # 'allauth.socialaccount.providers.facebook',
     # 'allauth.socialaccount.providers.google',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +65,10 @@ ROOT_URLCONF = 'ugly_mugs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
