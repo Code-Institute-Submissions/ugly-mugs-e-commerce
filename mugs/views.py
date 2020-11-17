250c4328f26b15dd2c62b6ef448ee2fed0d2e248
Robin Westerback <robinwesterback1989@gmail.com>
2020-11-17 16:14:24 +0000

    Add mugs views and templates

--- mugs/views.py
+++ mugs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Mugs
 
-# Create your views here.
+
+def mugs(request):
+    """ A view to show the collection page, including sorting and filtering """
+
+    mugs = Mugs.objects.all()
+
+    context = {
+        'mugs': mugs,
+    }
+
+    return render(request, 'mugs/mugs.html', context)
