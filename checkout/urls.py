b3c6e2f7b994150c4561fa8ec0ac6468d5a3b5cb
Robin Westerback <robinwesterback1989@gmail.com>
2020-11-18 21:25:19 +0000

    Add stripe elements and checkout-success views, urls and templates

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('view_cart', views.view_cart, name='view_cart'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('view_cart/', views.view_cart, name='view_cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
     #path('adjust/<item_id>/', views.adjust_cart, name='adjust_cart'),
     #path('remove/<item_id>/', views.remove_from_cart, name='remove_from_cart'),
