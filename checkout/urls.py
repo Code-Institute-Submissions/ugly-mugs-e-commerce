f9183cdac7377ff2304ec2fea6228167f869c907
Robin Westerback <robinwesterback1989@gmail.com>
2020-11-19 19:20:04 +0000

    Add checkout form data caching in payment intent

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,11 +4,12 @@ from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
     path('view_cart/', views.view_cart, name='view_cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
     #path('adjust/<item_id>/', views.adjust_cart, name='adjust_cart'),
     #path('remove/<item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('cache_checkout_data', views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +9,7 @@ from mugs.models import Mug
 from checkout.contexts import cart_contents
 
 import stripe
+import json
 
 
 def view_cart(request):
@@ -61,6 +63,23 @@ def add_to_cart(request, item_id):
 #
 #   except Exception as e:
 #        return HttpResponse(status=500)
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
