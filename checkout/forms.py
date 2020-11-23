75653f71f4b886b4c485957b6fb0aa47a5da2bcf
Robin Westerback <robinwesterback1989@gmail.com>
2020-11-23 21:17:22 +0000

    Add forms to profile

--- checkout/forms.py
+++ checkout/forms.py
@@ -12,7 +12,7 @@ class OrderForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         """
-        Add placeholders and classes, remove auto-generated
+        Add placeholders, remove auto-generated
         labels and set autofocus on first field
         """
         super().__init__(*args, **kwargs)
@@ -20,7 +20,6 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
