840a47d58626965103e4215fabe3db29e8e37172
Robin Westerback <robinwesterback1989@gmail.com>
2020-11-23 17:07:02 +0000

    Add profiles app

--- ugly_mugs/urls.py
+++ ugly_mugs/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('mugs/', include('mugs.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
