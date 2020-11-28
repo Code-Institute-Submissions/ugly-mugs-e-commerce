9920e01563070856086d58db1b2e971921623941
Robin Westerback <robinwesterback1989@gmail.com>
2020-11-28 14:29:52 +0000

    Add button for removing items from cart

--- checkout/views.py
+++ checkout/views.py
@@ -67,18 +67,21 @@ def adjust_cart(request, item_id):
     return redirect(reverse('view_cart'))
 
 
-#def remove_from_cart(request, item_id):
-#    """ Adjust the quantity of the specified mug to the specified amount """
-#
-#   try:
-#        cart = request.session.get('cart', {})
-#        cart.pop(item_id)
-#
-#        request.session['cart'] = cart
-#        return HttpResponse(status=200)
-#
-#   except Exception as e:
-#        return HttpResponse(status=500)
+def remove_from_cart(request, item_id):
+    """ Remove the item from the shopping cart """
+
+    try:
+        mug = get_object_or_404(Mug, pk=item_id)
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+        messages.success(request, f'Removed {mug.name} from your cart')
+
+        request.session['cart'] = cart
+        return redirect(reverse('view_cart'))
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
 
 
 @require_POST
